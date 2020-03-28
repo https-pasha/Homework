2397b8c40ca7a09fb8c63178f44a2ee1e6bdd70e
kreeptos <labime.infini@gmail.com>
2020-03-28 22:28:58 +0200

    lab_2

---
+++ lab_2/lab_2.1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#CD to /etc using absolute path
+	cd/etc/
+
+#CD to /home/ using absolute path:
+	cd/home/
+
+#From /home/ CD to ~/lab_tasks/2/example_folder using relative path
+	cd lab_tasks/2/example_folder
+
+#From ~/lab_tasks/2/example_folder/ CD to upper directory
+	cd ../
+
+#List files of ~/lab_tasks/2 using
+	ls ~/lab_tasks/
---
+++ lab_2/lab_2.2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#CD to users home using absolute path and print current directory
+	
+	cd/home/ && pwd
+
+#CD to this folder using absolute path and print current directory
+	????? i don't understand what you mean 
+
+#From users home CD to ~/lab_tasks/2/example_folder using relative ....
+	
+	cd ~/lab_tasks/2/example_folder && cd -
+
+#From users home list files of ~/lab_tasks/2 using relative path
+
+	cd ~/ && ls lab_tasks/2
