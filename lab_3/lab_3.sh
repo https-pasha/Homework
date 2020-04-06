611abb377d65c0efa3a8e77974281587aa8a5e47
kreeptos <labime.infini@gmail.com>
2020-04-06 19:36:16 +0300

    lab

---
+++ lab_3/lab_3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Junior level (3 points)
+
+
+ sudo cp ~/lab_tasks/3/files/* ~/labs/3/
+
+ cd ~/labs/3/
+ sudo mv file_1.txt file_1.mp3
+
+ sudo rm file_2.txt
+
+ sudo cp -r ~/lab_tasks/3/directory ~/labs/3/
+
+
+#Middle level (4 points)
+
+ sudo mv ~/labs/3/directory/*  ~/labs/3/
+
+ sudo cp file_3.txt ~/labs/3/directory/
+
+ sudo mv ~/labs/3/file_4.txt ~/labs/3/directory
+
+ 
+
+ ls -la ~/labs/3/
+
+ ls -la ~/labs/3/directory/
+
+
+#Senior level (5 points)
+
+ sudo mkdir -p ~/labs/3/a/b/c/
+ sudo  rm -r ~/labs/3/a
