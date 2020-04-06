ec1db41b5d890f11174e464d34f2dbec39110225
kreeptos <labime.infini@gmail.com>
2020-04-06 20:29:34 +0300

    lab

---
+++ lab_4/lab_4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Junior level (3 points)
+
+ cp -R ~/lab_tasks/4/* ~/labs/4/
+
+ rm -f ~/labs/4/to_remove/*
+
+ rm -f ~/labs/5/glob/*txt
+
+#Middle level (4 points)
+
+ cd ~/labs/4/
+
+ mv glob/* target/
+
+ touch target/new_file_{1..10}.txt
+
+ ls -ld ~/labs/4/glob/.*
+
+ cp -r target/. ~/labs/4/
+
+#Senior level (5 points)
+
+
