868a29eb06e9281ccd6e1b70472cb61d9600ed3f
kreeptos <kreeptos@gmail.com>
2020-05-13 18:02:17 +0300

    lab_5

---
+++ lab_5/lab_5.sh
@@ -0,0 +1,32 @@
+ #!/bin/bash
+ 
+ #Junior level (3 points)
+ 
+ man -f notify-send
+ 
+ man notify-send
+ 
+ notify-send --help
+ 
+ pinfo nano
+ 
+ #Middle level (4 points)
+ 
+ man -k ssh | wc -l
+ 
+ whatis nano
+ 
+ where nano
+ 
+ which nano
+ 
+ whereis nano
+
+ echo 'Команда which ищет приложение в пользовательской директории'
+ echo 'Команда whereis ищет программы среди стандартных директорий и флагов не имеет'
+ echo 'Whatis oтображает краткое описание какой-либо программы. Введите команду и название программы'
+ echo 'Where is a copy command WHICH'
+ 
+ ssh -V
+ 
+ 
