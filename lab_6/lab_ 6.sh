e067bc0875009d1cd19b45f0a9f41cb65601be02
kreeptos <kreeptos@gmail.com>
2020-05-13 20:58:33 +0300

    lab

--- lab_6/lab_ 6.sh
+++ lab_6/lab_ 6.sh
@@ -1,1 +1,29 @@
- 
+#!/bin/bash
+
+#Junior level (3 points)
+
+touch ~/labs/6/6.log
+
+echo "YOU_NAME" > ~/labs/6/6.log
+
+date >> ~/labs/6/6.log
+
+last -n 1 >> ~/labs/6/6.log
+
+cat ~/i_dont_exists.txt
+
+cat ~/i_dont_exists.txt 2> /dev/null
+
+cat ~/labs/6/6.log 2> /dev/null
+
+#Middle level (4 points)
+
+ping -c 10 i.ua >> ~/labs/6/6.log
+
+ping -c 10 i2fr3.ua >> ~/labs/6/6.errors
+
+echo $RANDOM >> ~/labs/6/6.log
+
+vim +3 ~/labs/6/6.log
+
+
--- lab_7/lab_7.sh
+++ lab_7/lab_7.sh
@@ -1,1 +1,39 @@
- 
+#!/bin/bash
+
+#Junior level (3 points)
+
+whoami
+
+id
+
+id root
+
+cat /etc/passwd
+
+cat /etc/passwd | wc -l
+
+cat /var/log/boot.log.1
+
+sudo cat /var/log/boot.log.1
+
+#Middle level (4 points)
+
+groups
+
+cat /etc/group | grep $USER | cut -d: -f1
+
+echo $SHELL
+
+sudo useradd -m -c "Vasya Testing User" 
+
+sudo touch /home/vasya/some.file
+
+sudo su - vasya -c "date>>/home/vasya/some.file"
+
+sudo chown vasya:root /home/vasya/some.file
+
+sudo su - vasya -c "date>>/home/vasya/some.file"
+
+sudo usermod -s /sbin/nologin vasya
+
+sudo userdel vasya
